eeab1bb924ec3d3706a559c9335404265976fc10
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-02-21 23:00:07 -0300

    Optimizacion del proyecto

--- Proyecto_Final/dc.py
+++ Proyecto_Final/dc.py
@@ -1,58 +1,67 @@
 # -*- coding: utf-8 -*-
 
+from math import *
 
-def calcularPorcentaje(parte, total):
+
+def calcularN(sbox, length_sbox):
     """
-    Calcula el porcentaje de una parte correspecto al total.
+    Dado un S-box calcula el numero maximo de bits necesarios.
     """
-    return (parte*100)/float(total)
+    return int(log(length_sbox)/log(2))
 
 
-def getPorcentajes(sbox, length_sbox, delta_x):
+def calcularDP(count, N):
     """
-    Obtiene los porcentajes del S-box de la fila correspondiente al delta_x.
+    Calcula la Probabilidad Diferencial de un eventos.
     """
-    x = range(length_sbox) # Lista: 0, 1, 2, ..., largo_sbox - 1
+    return count/float(2**N)
 
-    y = [] # Lista que contiene: Sbox(x)
-    x_prima = [] # Lista que contiene: x XOR delta_x
-    y_prima = [] # Lista que contiene: Sbox(x_prima)
+
+def calcularIguales(sbox, length_sbox, dx, dy, N):
+    """
+    Dado un S-box, dx (delta_x) y dy (deltas_y) calcula lo siguiente:
+        #{x Є Z_{2}^{N} : S(x) XOR S(x XOR dx) = dy}
+    """
+    x = range(length_sbox)
+
+    s_x = []
     for i in xrange(length_sbox):
-        y.append(sbox[i])
-        x_prima.append(x[i] ^ delta_x)
-        y_prima.append(sbox[x_prima[i]])
+        s_x.append(sbox[i])
 
-    delta_y = [] # Lista que contiene: y XOR y_prima
+    s_x_prima = []
     for i in xrange(length_sbox):
-        delta_y.append(y[i] ^ y_prima[i])
+        s_x_prima.append(sbox[x[i] ^ dx])
 
-    apariciones = [] # Lista que contiene las apariciones de cada numero
+    count = 0
     for i in xrange(length_sbox):
-        apariciones.append(delta_y.count(i))
+        tmp = s_x[i] ^ s_x_prima[i]
 
-    porcentajes = [] # Lista que contiene los porcentajes de los delta_y segun el delta_x
-    for i in xrange(length_sbox):
-        porcentajes.append(calcularPorcentaje(apariciones[i], length_sbox))
+        if tmp == dy:
+            count += 1
 
-    # Si la suma de los porcentajes de 100.0 devolver la lista
-    if sum(porcentajes) == 100.0:
-        return porcentajes
-        # return porcentajes[1:] # Me duelve toda la lista menos el primer elemento
-    # Sino devolver ### ERROR ###
-    else:
-        return "### ERROR ###"
+    return count
 
 
 def getTablaDiferencias(sbox, length_sbox):
     """
     Me devuelve la tabla de diferencias de un S-box.
     """
-    tabla = [] # Lista que contiene las listas de porcentajes
-    for delta_x in xrange(length_sbox):
-        # print delta_x, obtenerPorcentajes(sbox, length_sbox, delta_x)
-        tabla.append(getPorcentajes(sbox, length_sbox, delta_x))
+    n = calcularN(sbox, length_sbox)
 
-    return tabla
+    deltas_x = range(length_sbox)
+    deltas_y = range(length_sbox)
+
+    tabla_diferencias = []
+    for dx in deltas_x:
+        row = []
+        for dy in deltas_y:
+            count = calcularIguales(sbox, length_sbox, dx, dy, n)
+            dp = calcularDP(count, n)
+            row.append(dp)
+
+        tabla_diferencias.append(row)
+
+    return tabla_diferencias
 
 
 def printTabla(tabla, length_sbox):
@@ -62,37 +71,3 @@ def printTabla(tabla, length_sbox):
     print "ΔX \ ΔY", range(length_sbox)
     for i in xrange(length_sbox):
         print i, "    ", tabla[i]
-
-
-#------------------------------------------------------------------------------
-# S-boxes de prueba (estan en la carpeta, paginas 15 y 16)
-s1 = [3, 4, 5, 6, 7, 0, 1, 2]
-s2 = [6, 5, 2, 7, 3, 4, 1, 0]
-s3 = [6, 4, 1, 7, 0, 3, 5, 2]
-#------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-# S-boxes de Serpent
-sbox_0 = [3, 8, 15, 1, 10, 6, 5, 11, 14, 13, 4, 2, 7, 0, 9, 12]
-sbox_1 = [15, 12, 2, 7, 9, 0, 5, 10, 1, 11, 14, 8, 6, 13, 3, 4]
-sbox_2 = [8, 6, 7, 9, 3, 12, 10, 15, 13, 1, 14, 4, 0, 11, 5, 2]
-sbox_3 = [0, 15, 11, 8, 12, 9, 6, 3, 13, 1, 2, 4, 10, 7, 5, 14]
-sbox_4 = [1, 15, 8, 3, 12, 0, 11, 6, 2, 5, 4, 10, 9, 14, 7, 13]
-sbox_5 = [15, 5, 2, 11, 4, 10, 9, 12, 0, 3, 14, 8, 13, 6, 7, 1]
-sbox_6 = [7, 2, 12, 5, 8, 4, 6, 11, 14, 9, 1, 15, 13, 3, 10, 0]
-sbox_7 = [1, 13, 15, 0, 14, 8, 2, 11, 7, 4, 12, 10, 9, 3, 5, 6]
-#------------------------------------------------------------------------------
-
-# CheckSum
-# print sum(s1)
-# print sum(s2)
-# print sum(s3)
-# print "----------"
-# print sum(sbox_0)
-# print sum(sbox_1)
-# print sum(sbox_2)
-# print sum(sbox_3)
-# print sum(sbox_4)
-# print sum(sbox_5)
-# print sum(sbox_6)
-# print sum(sbox_7)
--- Proyecto_Final/lc.py
+++ Proyecto_Final/lc.py
@@ -3,13 +3,11 @@
 from math import *
 
 
-def calcularN(sbox):
+def calcularN(sbox, length_sbox):
     """
     Dado un S-box calcula el numero maximo de bits necesarios.
     """
-    m = len(sbox)
-
-    return int(log(m)/log(2))
+    return int(log(length_sbox)/log(2))
 
 
 def calcularBias(E, N):
@@ -17,17 +15,6 @@ def calcularBias(E, N):
     Calcula es Bias (Sesgo) de un evento E.
     """
     return (E - 2**(N-1))/float(2**N)
-
-
-def getBinary(number):
-    """
-    Genera una lista con las componentes binarias de un numero.
-    """
-    binary_list = list(bin(number)[2:])
-    # binary_list = binary_list[2:]
-    binary_list = [int(x) for x in binary_list]
-
-    return binary_list
 
 
 def getBinaryExtended(number, N):
@@ -93,7 +80,7 @@ def getTablaMascaras(sbox, length_sbox):
     """
     Me devuelve la tabla de mascaras de un S-box.
     """
-    n = calcularN(sbox)
+    n = calcularN(sbox, length_sbox)
 
     deltas = range(length_sbox)
     gammas = range(length_sbox)
@@ -118,23 +105,3 @@ def printTabla(tabla, length_sbox):
     print "Δ \ Γ", range(length_sbox)
     for i in xrange(length_sbox):
         print i, "  ", tabla[i]
-
-
-#------------------------------------------------------------------------------
-# S-boxes de prueba (estan en la carpeta, paginas 15 y 16)
-s1 = [3, 4, 5, 6, 7, 0, 1, 2]
-s2 = [6, 5, 2, 7, 3, 4, 1, 0]
-s3 = [6, 4, 1, 7, 0, 3, 5, 2]
-#------------------------------------------------------------------------------
-
-#------------------------------------------------------------------------------
-# S-boxes de Serpent
-sbox_0 = [3, 8, 15, 1, 10, 6, 5, 11, 14, 13, 4, 2, 7, 0, 9, 12]
-sbox_1 = [15, 12, 2, 7, 9, 0, 5, 10, 1, 11, 14, 8, 6, 13, 3, 4]
-sbox_2 = [8, 6, 7, 9, 3, 12, 10, 15, 13, 1, 14, 4, 0, 11, 5, 2]
-sbox_3 = [0, 15, 11, 8, 12, 9, 6, 3, 13, 1, 2, 4, 10, 7, 5, 14]
-sbox_4 = [1, 15, 8, 3, 12, 0, 11, 6, 2, 5, 4, 10, 9, 14, 7, 13]
-sbox_5 = [15, 5, 2, 11, 4, 10, 9, 12, 0, 3, 14, 8, 13, 6, 7, 1]
-sbox_6 = [7, 2, 12, 5, 8, 4, 6, 11, 14, 9, 1, 15, 13, 3, 10, 0]
-sbox_7 = [1, 13, 15, 0, 14, 8, 2, 11, 7, 4, 12, 10, 9, 3, 5, 6]
-#------------------------------------------------------------------------------
