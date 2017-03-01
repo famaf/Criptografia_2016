641b69c4645a6e1b225217657bb090c194de4780
Mario E. Ferreyra <ferreyramario7@gmail.com>
2017-03-01 14:07:29 -0300

    Cambios

--- Practico_Final/ejercicio_P1_7.py
+++ Practico_Final/ejercicio_P1_7.py
@@ -101,9 +101,9 @@ def main():
     print "###################################################################"
     print "# Bienvenido a la calculadora de Tabla de Diferencias de un S-box #"
     print "###################################################################\n"
-    # Si se desea probarlo con alguno de los S-box de Serpent, se debe cambiar
-    # la linea 110 del codigo por lo que se encuentra a la derecha del igual,
-    # es decir:
+    # Si se desea probarlo con alguno de los S-box de Serpent (se encuentran abajo del todo),
+    # se debe cambiarla linea 110 del codigo por lo que se encuentra a la derecha
+    # del igual, es decir:
     # Si queremos probarlo con el Sbox 0 de Serpent
     # Reemplazar "sbox = inputMode("Ingrese el S-box: ")"
     # Por: "sbox = [3, 8, 15, 1, 10, 6, 5, 11, 14, 13, 4, 2, 7, 0, 9, 12]"
